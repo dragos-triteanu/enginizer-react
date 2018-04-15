4f74f1b542e2c0da48fedcd559d9f82a7b0ed3e3
dragos.triteanu <dragos.triteanu@gmail.com>
2018-04-15 13:59:08 +0300

    Added react component lifecycle hooks for the CardComponent

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,21 +3,24 @@ import React, {Component} from 'react';
 import './Card.css';
 import Radium from "radium";
 
-class SomeComponent extends Component {
+class CardComponent extends Component {
   state = {
     showContent: true
   };
 
-  componentDidMount() {
-    this.props.onRef(this)
+  /**
+   *  #1 Never use side effects in the constructor such as http calls
+   */
+  constructor(props) {
+    console.log('[CardComponent] :: constructor');
+    super(props);
   }
 
-  componentWillUnmount() {
-    this.props.onRef(undefined)
-  }
-
+  /**
+   *  #2 Used for defining how the component should render
+   */
   render() {
-
+    console.log('[CardComponent] :: render');
     const styles = {
       headerStyle: {
         height: '50px',
@@ -50,9 +53,63 @@ class SomeComponent extends Component {
     );
   }
 
+  /**
+   *  #3 Executed after component is rendered. Don't update state, because it will force a re-render.
+   */
+  componentDidMount() {
+    console.log('[CardComponent] :: componentDidMount');
+    this.props.onRef(this)
+  }
+
+  /**
+   *  #4 Called immediately before a component is destroyed. Useful for cleanup
+   */
+  componentWillUnmount() {
+    console.log('[CardComponent] :: componentWillUnmount');
+    this.props.onRef(undefined)
+  }
+
+  /**
+   *  UPDATE #1:  Should return the new state in case of state change, otherwise should return null
+   */
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('[UPDATE CardComponent] :: getDerivedStateFromProps');
+    return null;
+  }
+
+  /**
+   *  UPDATE #2:
+   *  Executed after getDerivedStateFromProps.
+   *  Called to determine whether the change in props and state should trigger a re-render
+   *
+   *  THIS IS AUTOIMPLEMENTED BY THE FRAMEWORK. In order to make use of this function one should extend the
+   *  {@link PureComponent}.
+   */
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE CardComponent] UPDATE :: shouldComponentUpdate');
+    // If false if returned, the component will not be updates
+    return true;
+  }
+
+  /**
+   *  UPDATE #3:  Executed after shouldComponentUpdate
+   */
+  getSnapshotBeforeUpdate(nextProps, prevState) {
+    console.log('[UPDATE CardComponent] UPDATE :: getSnapshotBeforeUpdate');
+    return null;
+  }
+
+  /**
+   *  UPDATE #4:  Executed after getSnapshotBeforeUpdate.
+   *  Called after component has been updated
+   */
+  componentDidUpdate(){
+    console.log('[UPDATE CardComponent] UPDATE :: componentDidUpdate');
+  }
+
   toggleSubtitleVisibility() {
     this.setState({showContent: !this.state.showContent});
   }
 }
 
-export default Radium(SomeComponent);
+export default Radium(CardComponent);
